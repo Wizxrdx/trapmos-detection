51d40c617336e38a3ccd28c40f57764f7e8d5386
Wizxrdx <benedick.dumol@gmail.com>
2025-03-24 19:34:44 +0800

    moved to types/ dir

--- src/location.py
+++ src/location.py
@@ -1,11 +1,11 @@
-import location.wifi as wifi
-import location.gps as gps
+from src.types.wifi import WIFILocation
+from src.types.gps import GPSLocation
 
 
 class LocationManager():
     def __init__(self):
-        self.gps = gps.connect_gps()
-        self.wifi = wifi.connect_wifi()
+        self.gps = GPSLocation()
+        self.wifi = WIFILocation()
 
     def current_location(self):
         if self.gps.can_get_location():
