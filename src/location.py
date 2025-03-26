35b2ad8c97745cba08a3f107a67676f0145c39a6
Wizxrdx <benedick.dumol@gmail.com>
2025-03-26 15:24:03 +0800

    added a thread for gps

--- src/location.py
+++ src/location.py
@@ -4,14 +4,22 @@ from src.types.gps import GPSLocation
 
 class LocationManager():
     def __init__(self):
-        self.gps = GPSLocation()
-        self.wifi = WIFILocation()
+        self.__gps = GPSLocation(self)
+        self.__gps_latest_location = None
+        self.__wifi = WIFILocation(self)
+        self.__wifi_latest_location = None
+
+    def set_gps_location(self, location):
+        self.__gps_latest_location = location
+
+    def set_wifi_location(self, location):
+        self.__wifi_latest_location = location
 
     def current_location(self):
-        if self.gps.can_get_location():
-            return self.gps.current_location()
-        elif self.wifi.can_get_location():
-            return self.wifi.current_location()
+        if self.__gps.can_get_location():
+            return (self.__gps.__get_location())
+        elif self.__wifi.can_get_location():
+            return (self.__wifi.current_location())
 
     def close(self):
-        self.gps.close()
+        self.__gps.close()
--- src/types/gps.py
+++ src/types/gps.py
@@ -1,39 +1,47 @@
+import time
 import serial
 import pynmea2
+import threading
 
 class GPSLocation:
-    def __init__(self):
+    def __init__(self, manager):
         self.__gps = serial.Serial(
             port='/dev/ttyTHS1',
             baudrate=9600,
             timeout=0.5
         )
+        self.__manager = manager
+        self.__running = True
         print("GPS connected.")
 
-    def can_get_location(self):
+        self.__thread = threading.Thread(target=self.__run_loop)
+        self.__thread.start()
+
+    def __run(self):
+        while self.__running:
+            print("GPS is now running.")
+            lat, lon = self.__get_location()
+            if lat is not None:
+                self.__manager.set_gps_location(lat, lon)
+            time.sleep(2)
+
+    def __get_location(self):
         raw_data = self.__read_gps()
         parsed_data = self.__parse_data(raw_data)
         if parsed_data is not None:
-            return True
-        return False
+            print(parsed_data)
+            gga_lon = float(parsed_data['longitude'])
+            gga_lon_dir = parsed_data['longitude_dir']
+            gga_lat = float(parsed_data['latitude'])
+            gga_lat_dir = parsed_data['latitude_dir']
 
-    def current_location(self):
-        if self.can_get_location():
-            raw_data = self.__read_gps()
-            parsed_data = self.__parse_data(raw_data)
-            if parsed_data is not None:
-                print(parsed_data)
-                gga_lon = float(parsed_data['longitude'])
-                gga_lon_dir = parsed_data['longitude_dir']
-                gga_lat = float(parsed_data['latitude'])
-                gga_lat_dir = parsed_data['latitude_dir']
+            dec_lon = self.__gga_to_decimal(gga_lon, gga_lon_dir)
+            dec_lat = self.__gga_to_decimal(gga_lat, gga_lat_dir)
 
-                dec_lon = self.__gga_to_decimal(gga_lon, gga_lon_dir)
-                dec_lat = self.__gga_to_decimal(gga_lat, gga_lat_dir)
+            print(f'https://www.openstreetmap.org/search?query={dec_lon}%2C{dec_lat}')
 
-                print(f'https://www.openstreetmap.org/search?query={dec_lon}%2C{dec_lat}')
-
-                return (dec_lat, dec_lon)
+            return (dec_lat, dec_lon)
+        return (None, None)
 
     def close(self):
         self.__gps.close()
