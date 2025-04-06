81e300c88636467aa628bdedf609c591527af4db
Wizxrdx <benedick.dumol@gmail.com>
2025-04-06 23:18:09 +0800

    increased conf level

--- src/detection.py
+++ src/detection.py
@@ -24,7 +24,7 @@ def run_detection(dev_mode):
     model = YoloTRT(
         library="yolov7/build/libmyplugins.so",
         engine="yolov7/build/yolov7-tiny.engine",
-        conf=0.65,
+        conf=0.7,
         yolo_ver="v7"
     )
 
