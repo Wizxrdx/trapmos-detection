5381b0d417075815a2ace799dfaee5b4e6db2aca
Wizxrdx <benedick.dumol@gmail.com>
2025-04-13 02:03:25 +0800

    camera fix

--- app.py
+++ app.py
@@ -25,5 +25,13 @@ def main():
 
 if __name__ == "__main__":
     os.system('sudo ntpdate time.google.com')
-    os.system('bash camera.sh')
+    time.sleep(1)
+    os.system('v4l2-ctl -d /dev/video0 -c auto_exposure=1')
+    time.sleep(1)
+    os.system('v4l2-ctl -d /dev/video0 -c exposure_time_absolute=18')
+    time.sleep(1)
+    os.system('v4l2-ctl -d /dev/video0 -c focus_automatic_continuous=0')
+    time.sleep(1)
+    os.system('v4l2-ctl -d /dev/video0 -c focus_absolute=200')
+    time.sleep(1)
     main()
--- camera.sh
+++ camera.sh
@@ -5,8 +5,6 @@
 # Usage: sudo ./camera.sh
 # Set the camera to 640x480 resolution at 30 fps
 
-# sudo v4l2-ctl -d /dev/video0 -c gain=250
-# sleep 1
 v4l2-ctl -d /dev/video0 -c auto_exposure=1
 sleep 1
 v4l2-ctl -d /dev/video0 -c exposure_time_absolute=18
@@ -14,4 +12,3 @@ sleep 1
 v4l2-ctl -d /dev/video0 -c focus_automatic_continuous=0
 sleep 1
 v4l2-ctl -d /dev/video0 -c focus_absolute=200
-sleep 1
