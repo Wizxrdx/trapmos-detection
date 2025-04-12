9ef7fd4002d23d75a5813fe83e8d204869024a42
Wizxrdx <benedick.dumol@gmail.com>
2025-04-12 21:03:32 +0800

    fixed run time

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from datetime import datetime
 import time
 import os
 
-DEVICE_ID = "00000"
-DEVICE_NAME = f"TRAPMOS_{DEVICE_ID}"
-TIME_STARTED = int(time.time()) - 1640000000 + 60
-
 SLEEP_TIME = 60 * 1  # 29 minutes
 RUN_TIME = 60 * 0.2  # 1 minute
+
+DEVICE_ID = "00000"
+DEVICE_NAME = f"TRAPMOS_{DEVICE_ID}"
+TIME_STARTED = int(time.time()) - 1640000000 + RUN_TIME
 
 def main():
     from src.detection import run_detection
