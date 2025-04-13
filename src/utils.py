a0288e1d521507923ff9c41dcfd20484f04905b6
Wizxrdx <benedick.dumol@gmail.com>
2025-04-13 23:01:58 +0800

    added functions to filter out background on image

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import socket
 import subprocess
+import numpy as np
+import cv2
 
 # Get IP address
 def get_ip():
@@ -19,3 +21,23 @@ def get_wifi():
         return result if result else "Disconnected"
     except:
         return "Disconnected"
+
+def focus_on_circle(img, radius=100):
+    h, w = img.shape[:2]
+
+    black_image = np.zeros_like(img)
+
+    mask = np.zeros((h, w), dtype=np.uint8)
+    center = (w // 2, h // 2)
+
+    cv2.circle(mask, center, radius, (255), thickness=-1)
+
+    circular_focus = cv2.bitwise_and(img, img, mask=mask)
+
+    return circular_focus
+
+def snip_sides(img, left_crop=50, right_crop=50):
+    h, w = img.shape[:2]
+    cropped = img[:, left_crop:w - right_crop]
+    return cropped
+
