92df031bfd7d24f1de07e9439907933e70d2ab06
Wizxrdx <benedick.dumol@gmail.com>
2025-04-14 12:40:55 +0800

    increased conf level

--- src/detection.py
+++ src/detection.py
@@ -12,7 +12,7 @@ import app
 
 def run_detection(dev_mode, oled=None):
     # Initialize YOLO model
-    model = YoloONNX("trapmos.onnx", conf_threshold=0.25)
+    model = YoloONNX("trapmos.onnx", conf_threshold=0.6)
 
     # Initialize location manager
     print("Initializing location manager...")
