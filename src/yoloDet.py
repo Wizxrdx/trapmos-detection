54ead9b4f0942b09e1c4430a12482ebe13c70094
Wizxrdx <benedick.dumol@gmail.com>
2025-05-08 12:46:57 +0800

    initial commit

--- src/yoloDet.py
+++ src/yoloDet.py
@@ -8,7 +8,8 @@ class YoloONNX:
         self.session = ort.InferenceSession(model_path, providers=['CPUExecutionProvider'])
         self.input_name = self.session.get_inputs()[0].name
         self.conf_threshold = conf_threshold
-        self.class_names = ["Aedes Mosquito", "Aedes Mosquito", "Aedes Mosquito"]
+        self.class_names = ["Culex Mosquito", "Aedes Mosquito", "Aedes Mosquito"]
+        # self.class_names = ["Aedes Mosquito", "Aedes Mosquito", "Aedes Mosquito"]
         self.colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
 
     def infer(self, img):
