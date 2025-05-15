22a3d1931ebe743a2ebbb35b12cbcad01616e18a
Wizxrdx <benedick.dumol@gmail.com>
2025-05-15 10:29:01 +0800

    adjust camera settings

--- src/detection.py
+++ src/detection.py
@@ -66,8 +66,8 @@ def run_detection(dev_mode, oled=None):
                 # cap.set(cv2.CAP_PROP_EXPOSURE, 50)
 
                 cap.set(cv2.CAP_PROP_FOCUS, 160)
-                cap.set(cv2.CAP_PROP_GAIN, 40)
-                cap.set(cv2.CAP_PROP_EXPOSURE, 40)
+                cap.set(cv2.CAP_PROP_GAIN, 50)
+                cap.set(cv2.CAP_PROP_EXPOSURE, 50)
 
                 if frame_counter % skip_frames == 0:
                     frame = focus_on_circle(frame, 280)
